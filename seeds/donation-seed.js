8dfcd5300f4178ad477f62a6f3a4ec196c0cfdae
Bradley Le <brabrele@gmail.com>
2022-02-11 11:17:05 -0800

    added donation seed data in donation-seed.js

---
+++ seeds/donation-seed.js
@@ -0,0 +1,19 @@
+const { Donation } = require('../models');
+
+const donationData = [
+    {
+        user_id: 2,
+        project_id: 1,
+        amount: 20,
+    },
+    {
+        user_id: 3,
+        project_id: 2,
+        amount: 40,
+    },
+    {
+        user_id: 1,
+        project_id: 3,
+        amount: 30,
+    },
+];
