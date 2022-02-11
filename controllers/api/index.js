0888f9faf2eb3393b5ada6f57508aa2c34168045
Baofeng Guo <peterguo1983@gmail.com>
2022-02-11 10:28:43 -0800

    Updated the index.js.

---
+++ controllers/api/index.js
@@ -0,0 +1,16 @@
+// Dependencies
+const router = require('express').Router();
+// API routes folder
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
