b8ba8a7e758f628d00fc3b219b470fc105bb5b6d
Baofeng Guo <peterguo1983@gmail.com>
2022-02-11 14:37:08 -0800

    Changed 'name' to 'title'

--- seeds/project-seeds.js
+++ seeds/project-seeds.js
@@ -3,19 +3,19 @@ const { Project } = require('../models');
 const projectData = [
     {
         user_id: 1,
-        name: "My Dog's Vet Bills",
+        title: "My Dog's Vet Bills",
         description: "The vet bills for my dog are expensive, so any amount will help!",
         fund_needed: 400,
     },
     {
         user_id: 2,
-        name: "New Desk",
+        title: "New Desk",
         description: "I don't earn enough to buy a new desk. I use a plastic table from Walmart.",
         fund_needed: 200,
     },
     {
         user_id: 3,
-        name: "New Work Shoes",
+        title: "New Work Shoes",
         description: "A friend of mine has old work shoes that are warn out. They're a sweet person, so I believe they deserve an upgrade!",
         fund_needed: 60,
     },
