dbeaea4ba3728a94cf1e708f433c1278f95d50cd
Bradley Le <brabrele@gmail.com>
2022-02-11 00:41:04 -0800

    added if statement to use the JAWSDB_URL if it exists, is used for heroku deployment, created a module export

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,10 @@
 const Sequelize = require('sequelize');
 require('.dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+
+module.exports = sequelize;
