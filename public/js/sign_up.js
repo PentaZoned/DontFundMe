ded588423c514fe2cce9dd880c223369b450ca03
Bradley Le <brabrele@gmail.com>
2022-02-12 20:16:06 -0800

    added an event listener to the form in the sign up page, so far the function just takes the user inputs

---
+++ public/js/sign_up.js
@@ -0,0 +1,11 @@
+const signUp = () => async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    
+};
+
+document.querySelector('#signInButton').addEventListener('submit', signUp);
